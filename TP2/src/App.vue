150a29f58ab0e156a68b55ea5cbc115d10d470b0
Augusto Ospal <49209526+AugustoOspal@users.noreply.github.com>
2025-10-01 19:48:18 -0300

    Tailwind funcionando

--- TP2/src/App.vue
+++ TP2/src/App.vue
@@ -3,15 +3,9 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Click Me
+  </button>
 </template>
 
 <style scoped>
